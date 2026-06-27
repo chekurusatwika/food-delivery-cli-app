48e90be510c6a5985317d524d867c36b95d36a5d
chekuru Satwika <chekurusatwika@gmail.com>
2026-06-28 00:17:45 +0530

    feat: get the dishes list from csvReader class to DishesRepository

---
+++ food-delivery-cli-app/src/com/fooddeliveryapp/repository/DishesRepository.java
@@ -0,0 +1,21 @@
+package com.fooddeliveryapp.repository;
+
+import com.fooddeliveryapp.model.Dishes;
+import com.fooddeliveryapp.util.CsvReader;
+
+import java.util.List;
+
+public class DishesRepository {
+    private List<Dishes> dishesList;
+
+    public DishesRepository(){
+        CsvReader csvReader = new CsvReader();
+        this.dishesList = csvReader.readDishesFromCsv();
+
+    }
+
+    //get all the dishes
+    public List<Dishes> getAllDishes(){
+        return dishesList;
+    }
+}
