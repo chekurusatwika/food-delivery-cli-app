9d89b9827a61ffbda8692ed2ccb3aaef36adc7e7
chekuru Satwika <chekurusatwika@gmail.com>
2026-06-28 00:23:58 +0530

    test: verify that the actual data came to their corresponding repositories

--- food-delivery-cli-app/src/Main.java
+++ food-delivery-cli-app/src/Main.java
@@ -1,10 +1,22 @@
+import com.fooddeliveryapp.repository.CustomerRepository;
+import com.fooddeliveryapp.repository.DishesRepository;
+import com.fooddeliveryapp.repository.RestaurantRepository;
 import com.fooddeliveryapp.util.CsvReader;
 
 public class Main {
     public static void main(String[] args) {
         CsvReader csvReader = new CsvReader();
-        System.out.println(csvReader.readCustomersFromCsv());
+       /* System.out.println(csvReader.readCustomersFromCsv());
         System.out.println(csvReader.readDishesFromCsv());
-        System.out.println(csvReader.readRestaurantsFromCsv());
+        System.out.println(csvReader.readRestaurantsFromCsv()); */
+        CustomerRepository customerRepository = new CustomerRepository();
+        System.out.println(customerRepository.getAllCustomers());
+
+        DishesRepository dishesRepository = new DishesRepository();
+        System.out.println(dishesRepository.getAllDishes());
+
+        RestaurantRepository restaurantRepository = new RestaurantRepository();
+        System.out.println(restaurantRepository.getAllRestaurants());
+
     }
 }
