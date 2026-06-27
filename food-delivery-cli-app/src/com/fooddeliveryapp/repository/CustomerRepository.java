d42fe4638051c7d3eb42d65e800cf1fd41bbbc87
chekuru Satwika <chekurusatwika@gmail.com>
2026-06-28 01:18:02 +0530

    feat: add new customer and check customer already exsists with customer id

--- food-delivery-cli-app/src/com/fooddeliveryapp/repository/CustomerRepository.java
+++ food-delivery-cli-app/src/com/fooddeliveryapp/repository/CustomerRepository.java
@@ -4,6 +4,7 @@ import com.fooddeliveryapp.model.Customer;
 import com.fooddeliveryapp.util.CsvReader;
 
 import java.util.List;
+import java.util.Optional;
 
 public class CustomerRepository {
     private List<Customer> customersList;
@@ -12,8 +13,18 @@ public class CustomerRepository {
         CsvReader csvReader = new CsvReader();
         this.customersList = csvReader.readCustomersFromCsv();
     }
-
+    // get all customers
     public List<Customer> getAllCustomers(){
         return customersList;
+    }
+    //CURD OPERATIONS
+    //1.add customer
+    public Customer addCustomer(Customer customer){
+        this.customersList.add(customer);
+        return customer;
+    }
+    //2.get customer by id
+    public Optional<Customer> getCustomerById(String id){
+         return customersList.stream().filter(customer -> customer.getId().equals(id)).findFirst();
     }
 }
