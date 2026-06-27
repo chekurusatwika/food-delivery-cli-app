446e0ed4a42d122abf7ecc616c3dcdd9e142d4d7
chekuru Satwika <chekurusatwika@gmail.com>
2026-06-27 23:25:50 +0530

    test: verify dishes data loading from csv

--- food-delivery-cli-app/src/Main.java
+++ food-delivery-cli-app/src/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         CsvReader csvReader = new CsvReader();
         System.out.println(csvReader.readCustomersFromCsv());
+        System.out.println(csvReader.readDishesFromCsv());
     }
 }
