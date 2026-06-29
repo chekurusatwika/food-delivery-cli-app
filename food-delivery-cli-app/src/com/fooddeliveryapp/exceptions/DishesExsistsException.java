24c341c09ad2c65c4ccf918939697643d2ce6a56
chekuru Satwika <chekurusatwika@gmail.com>
2026-06-29 23:36:17 +0530

    feat: write custom exception for already exsisting dish

---
+++ food-delivery-cli-app/src/com/fooddeliveryapp/exceptions/DishesExsistsException.java
@@ -0,0 +1,7 @@
+package com.fooddeliveryapp.exceptions;
+
+public class DishesExsistsException extends Exception {
+    public DishesExsistsException(String message) {
+        super(message);
+    }
+}
