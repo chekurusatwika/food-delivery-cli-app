b7dd633633429cf6aa352b54b8761fd3eda7797d
chekuru Satwika <chekurusatwika@gmail.com>
2026-06-29 23:39:20 +0530

    feat: implement add dishes method and get dish by id

--- food-delivery-cli-app/src/com/fooddeliveryapp/repository/DishesRepository.java
+++ food-delivery-cli-app/src/com/fooddeliveryapp/repository/DishesRepository.java
@@ -4,6 +4,7 @@ import com.fooddeliveryapp.model.Dishes;
 import com.fooddeliveryapp.util.CsvReader;
 
 import java.util.List;
+import java.util.Optional;
 
 public class DishesRepository {
     private List<Dishes> dishesList;
@@ -16,6 +17,7 @@ public class DishesRepository {
 
     //get all the dishes
     public List<Dishes> getAllDishes(){
+
         return dishesList;
     }
 
@@ -23,5 +25,10 @@ public class DishesRepository {
     public Dishes addDish(Dishes dish){
         this.dishesList.add(dish);
         return dish;
+    }
+
+    //2.get dishes by id
+    public Optional<Dishes> getDishById(String id){
+        return dishesList.stream().filter(dish -> dish.getId().equals(id)).findFirst();
     }
 }
