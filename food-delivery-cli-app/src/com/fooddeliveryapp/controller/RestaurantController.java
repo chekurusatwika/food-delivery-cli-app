72709ae2774fe10490e951114137724090819d17
chekuru Satwika <chekurusatwika@gmail.com>
2026-06-30 22:49:05 +0530

    feat: implement get all restuarants and get restaurant by id

--- food-delivery-cli-app/src/com/fooddeliveryapp/controller/RestaurantController.java
+++ food-delivery-cli-app/src/com/fooddeliveryapp/controller/RestaurantController.java
@@ -1,8 +1,11 @@
 package com.fooddeliveryapp.controller;
 
+import com.fooddeliveryapp.exceptions.RestaurantNotFoundException;
 import com.fooddeliveryapp.exceptions.RestuarantExsistaException;
 import com.fooddeliveryapp.model.Restaurant;
 import com.fooddeliveryapp.service.RestaurantService;
+
+import java.util.List;
 
 public class RestaurantController {
     private RestaurantService restaurantService;
@@ -11,5 +14,13 @@ public class RestaurantController {
     }
     public Restaurant addRestaurant(Restaurant restaurant) throws RestuarantExsistaException {
          return this.restaurantService.addRestaurant(restaurant);
+    }
+    //get all restaurants
+    public List<Restaurant> getAllRestaurants() {
+        return this.restaurantService.getAllRestaurants();
+    }
+    //get restaurant by id
+    public Restaurant getRestaurantById(String id) throws RestaurantNotFoundException {
+        return this.restaurantService.getRestaurantById(id);
     }
 }
