a183b80dd1ee6cfb264584f03df0fcb6ba207a05
chekuru Satwika <chekurusatwika@gmail.com>
2026-06-30 16:32:08 +0530

    feat: implement displayDishesList functionality and formatting the console data.

--- food-delivery-cli-app/src/com/fooddeliveryapp/ui/CustomerMenu.java
+++ food-delivery-cli-app/src/com/fooddeliveryapp/ui/CustomerMenu.java
@@ -1,12 +1,19 @@
 package com.fooddeliveryapp.ui;
 
 import com.fooddeliveryapp.controller.CustomerController;
+import com.fooddeliveryapp.controller.DishesController;
 import com.fooddeliveryapp.exceptions.CustomerExsistsException;
 import com.fooddeliveryapp.model.Customer;
+import com.fooddeliveryapp.model.Dishes;
 import com.fooddeliveryapp.repository.CustomerRepository;
+import com.fooddeliveryapp.repository.DishesRepository;
 import com.fooddeliveryapp.service.CustomerServiceImpl;
+import com.fooddeliveryapp.service.DishesService;
+import com.fooddeliveryapp.service.DishesServiceImpl;
 import com.fooddeliveryapp.util.Factory;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class CustomerMenu {
@@ -15,15 +22,16 @@ public class CustomerMenu {
     public void displayCustomerMenu(){
         Scanner sc = new Scanner(System.in);
         while(true) {
-            System.out.println("-------------------------------------------------------------");
+            /*System.out.println("-------------------------------------------------------------");
             System.out.println("               FOOD DELIVERY APP                             ");
-            System.out.println("-------------------------------------------------------------");
+            System.out.println("-------------------------------------------------------------");*/
+            displayMenuHeader("WELCOME TO FOOD DELIVERY APP");
             System.out.println();
             System.out.println("please select one option: ");
             System.out.println("-------------------------------------------------------------");
             System.out.println("1.Register(new customer) ");
             System.out.println("2.Login ( already exsisting customer)");
-            System.out.println("3. View Menu");
+            System.out.println("3. View Dishes Menu");
             System.out.println("4. view Restaurants");
             System.out.println("5. Place Orders");
             System.out.println("6. View Orders");
@@ -35,6 +43,9 @@ public class CustomerMenu {
                 case 1:
                     displayRegisterMenu();
                     break;
+                case 3:
+                    displayDishesList() ;
+                    break;
                 case 7:
                     System.out.println("thankyou plz visit again");
                     System.exit(0);
@@ -45,6 +56,23 @@ public class CustomerMenu {
 
 
         }
+    }
+
+    private void displayDishesList() {
+
+        /*DishesRepository dishesRepository = new DishesRepository();
+        DishesServiceImpl dishesService = new DishesServiceImpl(dishesRepository);
+        DishesController dishesController = new DishesController(dishesService);*/
+        DishesController dishesController = Factory.getDishesController();
+        List<Dishes> dishesList = dishesController.getAllDishes();
+        //System.out.println(dishesController.getAllDishes());
+        String dashLines = new String(new char[150]).replace("\0", "-");
+        displayMenuHeader("Menu Items");
+        System.out.format("%-10s %-30s %-80s %-10s\n","Id","Name","Description","Price");
+        System.out.println(dashLines);
+        dishesList.forEach(dishes -> {
+            System.out.printf("%-10s %-30s %-80s %-10s\n",dishes.getId(),dishes.getName(),dishes.getDescription(),String.format("$%.2f",dishes.getPrice()));
+        });
     }
 
     public void displayRegisterMenu(){
@@ -90,5 +118,16 @@ public class CustomerMenu {
             displayCustomerMenu();
 
         }
+    }
+    public void displayMenuHeader(String menuHeader){
+        // u have a string constructor which has char arr which contains\0 by default
+        String dashLines = new String(new char [150]).replace("\0", "-");
+        System.out.println(dashLines);
+        String spaces = new String(new char[70]).replace("\0", " ");
+        System.out.format("%-70s %-10s %-70s \n", spaces,menuHeader,spaces);
+        //here "-" is for left alignment
+        System.out.println(dashLines);
+
+
     }
 }
