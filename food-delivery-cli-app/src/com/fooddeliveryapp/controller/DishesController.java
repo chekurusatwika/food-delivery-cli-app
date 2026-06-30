c36dd8920ac5e3d89bc8db9f4f72cd020b6b029f
chekuru Satwika <chekurusatwika@gmail.com>
2026-06-30 16:26:56 +0530

    feat:  implement add dishes and get dish by id methods

--- food-delivery-cli-app/src/com/fooddeliveryapp/controller/DishesController.java
+++ food-delivery-cli-app/src/com/fooddeliveryapp/controller/DishesController.java
@@ -1,17 +1,32 @@
 package com.fooddeliveryapp.controller;
 
+import com.fooddeliveryapp.exceptions.DishNotFoundException;
 import com.fooddeliveryapp.exceptions.DishesExsistsException;
 import com.fooddeliveryapp.model.Dishes;
 import com.fooddeliveryapp.service.DishesServiceImpl;
 
+import java.util.List;
+
 public class DishesController {
     private DishesServiceImpl dishesService;
     public DishesController(DishesServiceImpl dishesService){
+
         this.dishesService = dishesService;
+    }
+
+    //get all dishes
+    public List<Dishes> getAllDishes(){
+        return dishesService.getAllDishes();
     }
 
     //add dishes
     public Dishes addDishes(Dishes dishes)throws DishesExsistsException {
         return  this.dishesService.addDishes(dishes);
     }
+    //get dish by id
+    public Dishes getDishesById(String id) throws DishNotFoundException {
+        return this.dishesService.getDishesById(id);
+    }
+
 }
+
