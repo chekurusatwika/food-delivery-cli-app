982d1745a95b9c903462b0cb29fc2368cfe1104e
chekuru Satwika <chekurusatwika@gmail.com>
2026-06-30 22:48:56 +0530

    feat: implement get all restuarants and get restaurant by id

--- food-delivery-cli-app/src/com/fooddeliveryapp/service/RestaurantService.java
+++ food-delivery-cli-app/src/com/fooddeliveryapp/service/RestaurantService.java
@@ -1,9 +1,16 @@
 package com.fooddeliveryapp.service;
 
+import com.fooddeliveryapp.exceptions.RestaurantNotFoundException;
 import com.fooddeliveryapp.exceptions.RestuarantExsistaException;
 import com.fooddeliveryapp.model.Restaurant;
+
+import java.util.List;
 
 public interface RestaurantService {
 
     public Restaurant addRestaurant(Restaurant restaurant) throws RestuarantExsistaException;
+    // get all restuarants
+    public List<Restaurant> getAllRestaurants();
+    // get restuarant by id
+    public Restaurant getRestaurantById(String id) throws RestaurantNotFoundException;
 }
--- food-delivery-cli-app/src/com/fooddeliveryapp/service/RestaurantServiceImpl.java
+++ food-delivery-cli-app/src/com/fooddeliveryapp/service/RestaurantServiceImpl.java
@@ -1,10 +1,12 @@
 package com.fooddeliveryapp.service;
 
+import com.fooddeliveryapp.exceptions.RestaurantNotFoundException;
 import com.fooddeliveryapp.exceptions.RestuarantExsistaException;
 import com.fooddeliveryapp.model.Restaurant;
 import com.fooddeliveryapp.repository.DishesRepository;
 import com.fooddeliveryapp.repository.RestaurantRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public class RestaurantServiceImpl implements RestaurantService {
@@ -12,6 +14,7 @@ public class RestaurantServiceImpl implements RestaurantService {
     public RestaurantServiceImpl(RestaurantRepository restaurantRepository) {
         this.restaurantRepository = restaurantRepository;
     }
+    //add restaurant
     @Override
     public Restaurant addRestaurant(Restaurant restaurant) throws RestuarantExsistaException {
         Optional<Restaurant> restaurantById = restaurantRepository.getRestaurantById(restaurant.getId());
@@ -19,5 +22,19 @@ public class RestaurantServiceImpl implements RestaurantService {
             throw new RestuarantExsistaException("resturant already exsists with this id"+restaurant.getId());
         }
         return restaurantRepository.addRestaurant(restaurant);
+    }
+
+    @Override
+    public List<Restaurant> getAllRestaurants() {
+        return restaurantRepository.getAllRestaurants();
+    }
+
+    @Override
+    public Restaurant getRestaurantById(String id) throws RestaurantNotFoundException {
+        Optional<Restaurant> restaurantById = restaurantRepository.getRestaurantById(id);
+        if(!restaurantById.isPresent()){
+            throw new RestaurantNotFoundException("restaurant not found with this id"+id);
+        }
+        return restaurantById.get();
     }
 }
