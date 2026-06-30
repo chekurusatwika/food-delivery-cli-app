f578321855b0901bf4367dd2f36ed947f68fda02
chekuru Satwika <chekurusatwika@gmail.com>
2026-06-30 22:47:47 +0530

    feat: implement obj ready methods for Restaurant repository ,service and controller

--- food-delivery-cli-app/src/com/fooddeliveryapp/util/Factory.java
+++ food-delivery-cli-app/src/com/fooddeliveryapp/util/Factory.java
@@ -2,11 +2,14 @@ package com.fooddeliveryapp.util;
 
 import com.fooddeliveryapp.controller.CustomerController;
 import com.fooddeliveryapp.controller.DishesController;
+import com.fooddeliveryapp.controller.RestaurantController;
 import com.fooddeliveryapp.repository.CustomerRepository;
 import com.fooddeliveryapp.repository.DishesRepository;
+import com.fooddeliveryapp.repository.RestaurantRepository;
 import com.fooddeliveryapp.service.CustomerService;
 import com.fooddeliveryapp.service.CustomerServiceImpl;
 import com.fooddeliveryapp.service.DishesServiceImpl;
+import com.fooddeliveryapp.service.RestaurantServiceImpl;
 
 public class Factory {
     // static methods
@@ -38,4 +41,16 @@ public class Factory {
     public static DishesController getDishesController() {
         return new DishesController(getDishesService());
     }
+    //--------------------------------------------------------------------------------------------------
+    // FOR RESTAURANT
+    public static RestaurantRepository getRestaurantRepository() {
+        return new RestaurantRepository();
+    }
+    public static RestaurantServiceImpl getRestaurantService() {
+        return new RestaurantServiceImpl(getRestaurantRepository());
+    }
+    public static RestaurantController getRestaurantController() {
+        return new RestaurantController(getRestaurantService());
+    }
+
 }
