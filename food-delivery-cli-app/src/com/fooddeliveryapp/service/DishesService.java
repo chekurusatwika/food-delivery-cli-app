2699bb8c338690604ec70a5ee75824836613cc8d
chekuru Satwika <chekurusatwika@gmail.com>
2026-06-30 16:28:58 +0530

    feat:  write addDishes and getDishById abstract methods

--- food-delivery-cli-app/src/com/fooddeliveryapp/service/DishesService.java
+++ food-delivery-cli-app/src/com/fooddeliveryapp/service/DishesService.java
@@ -1,9 +1,16 @@
 package com.fooddeliveryapp.service;
 
+import com.fooddeliveryapp.exceptions.DishNotFoundException;
 import com.fooddeliveryapp.exceptions.DishesExsistsException;
 import com.fooddeliveryapp.model.Dishes;
 
+import java.util.List;
+
 public interface DishesService {
 
+
+    public List<Dishes> getAllDishes();
     public Dishes addDishes(Dishes dishes)throws DishesExsistsException;
+
+    public Dishes getDishesById(String id) throws DishNotFoundException;
 }
