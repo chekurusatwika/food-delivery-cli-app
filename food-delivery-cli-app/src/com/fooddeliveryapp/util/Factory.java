5fbec973bb89a8e66db1831f5a4bcfa57c6c855d
chekuru Satwika <chekurusatwika@gmail.com>
2026-06-30 16:33:29 +0530

    feat: implement obj ready methods for Dishes repository ,service and controller

--- food-delivery-cli-app/src/com/fooddeliveryapp/util/Factory.java
+++ food-delivery-cli-app/src/com/fooddeliveryapp/util/Factory.java
@@ -1,9 +1,12 @@
 package com.fooddeliveryapp.util;
 
 import com.fooddeliveryapp.controller.CustomerController;
+import com.fooddeliveryapp.controller.DishesController;
 import com.fooddeliveryapp.repository.CustomerRepository;
+import com.fooddeliveryapp.repository.DishesRepository;
 import com.fooddeliveryapp.service.CustomerService;
 import com.fooddeliveryapp.service.CustomerServiceImpl;
+import com.fooddeliveryapp.service.DishesServiceImpl;
 
 public class Factory {
     // static methods
@@ -19,5 +22,20 @@ public class Factory {
     //controller
     public static CustomerController getCustomerController() {
         return new CustomerController(getCustomerService());
+    }
+    // csv reader obj
+    public static CsvReader getCsvReader() {
+        return new CsvReader();
+    }
+    //--------------------------------------------------------------------------------------------
+    //for dish
+    public static DishesRepository getDishesRepository() {
+        return new DishesRepository();
+    }
+    public static DishesServiceImpl getDishesService() {
+        return new DishesServiceImpl(getDishesRepository());
+    }
+    public static DishesController getDishesController() {
+        return new DishesController(getDishesService());
     }
 }
