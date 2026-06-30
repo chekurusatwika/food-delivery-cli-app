f7e4fcce421be47a5b5d4718a46795099190d63f
chekuru Satwika <chekurusatwika@gmail.com>
2026-06-30 16:30:42 +0530

    feat: provide method definations for adddish and get dish by id

--- food-delivery-cli-app/src/com/fooddeliveryapp/service/DishesServiceImpl.java
+++ food-delivery-cli-app/src/com/fooddeliveryapp/service/DishesServiceImpl.java
@@ -1,9 +1,11 @@
 package com.fooddeliveryapp.service;
 
+import com.fooddeliveryapp.exceptions.DishNotFoundException;
 import com.fooddeliveryapp.exceptions.DishesExsistsException;
 import com.fooddeliveryapp.model.Dishes;
 import com.fooddeliveryapp.repository.DishesRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public class DishesServiceImpl implements DishesService {
@@ -14,11 +16,26 @@ public class DishesServiceImpl implements DishesService {
     }
 
     @Override
+    public List<Dishes> getAllDishes() {
+        return dishesRepository.getAllDishes();
+    }
+
+    @Override
     public Dishes addDishes(Dishes dishes) throws DishesExsistsException {
         Optional<Dishes> dishById = dishesRepository.getDishById(dishes.getId());
         if(dishById.isPresent()){
             throw new DishesExsistsException("this dish already exsists with id"+dishes.getId());
         }
         return this.dishesRepository.addDish(dishes);
+    }
+
+
+    @Override
+    public Dishes getDishesById(String id) throws DishNotFoundException {
+        Optional<Dishes> dishById = dishesRepository.getDishById(id);
+        if(!dishById.isPresent()){
+            throw new DishNotFoundException("this dish does not exist"+id);
+        }
+        return  dishById.get(); // watever the dish with the given id that  obj will be printed
     }
 }
